4247741603b4d0d11b6194d6b7b9f15b92a3f856
Dani Portela <daniel.portela96@hotmail.com>
2023-06-29 11:06:26 -0300

    Set up initial Grafbase schemas

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,23 @@
 // Components
 import Image from "next/image"
+import Link from "next/link"
 
-const FooterColumn = () => (
+// Constants
+import { footerLinks } from "@/constants"
+
+// Types
+type ColumnProps = {
+  title: string,
+  links: Array<string>
+}
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="footer_column">
-    <h4 className="font-semibold">Title</h4>
+    <h4 className="font-semibold">{title}</h4>
     <ul className="flex flex-col gap-2 font-normal">
-      Links
+      { links.map(link => (
+        <Link key={link} href="/">{link}</Link>
+      )) }
     </ul>
   </div>
 )
@@ -28,8 +40,29 @@ const Footer = () => {
         </div>
 
         <div className="flex flex-wrap gap-12">
-          <FooterColumn />
+          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links} />
+            <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links} />
+          </div>
+
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links} />
+          
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links} />
+            <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links} />
+          </div>
+
+            <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links} />
         </div>
+      </div>
+
+      <div className="flex-between footer_copyright">
+        <p>@ 2023 Drizzzle. All rights reserved</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">10,214</span> projects submitted
+        </p>
       </div>
     </footer>
   )
